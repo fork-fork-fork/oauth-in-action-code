5dcd93e6328894f4ee268ef53a3aeb2fe1ccb62f
kskim <ks.kim@sqisoft.com>
2024-03-15 11:14:31 +0900

    read, write, delete

--- exercises/ch-4-ex-2/protectedResource.js
+++ exercises/ch-4-ex-2/protectedResource.js
@@ -32,20 +32,20 @@ var getAccessToken = function(req, res, next) {
 	} else if (req.query && req.query.access_token) {
 		inToken = req.query.access_token
 	}
-	
+
 	console.log('Incoming token: %s', inToken);
 	nosql.one().make(function(builder) {
-	  builder.where('access_token', inToken);
-	  builder.callback(function(err, token) {
-	    if (token) {
-	      console.log("We found a matching token: %s", inToken);
-	    } else {
-	      console.log('No matching token was found.');
-	    };
-	    req.access_token = token;
-	    next();
-	    return;
-	  });
+		builder.where('access_token', inToken);
+		builder.callback(function(err, token) {
+			if (token) {
+				console.log("We found a matching token: %s", inToken);
+			} else {
+				console.log('No matching token was found.');
+			};
+			req.access_token = token;
+			next();
+			return;
+		});
 	});
 };
 
@@ -59,35 +59,42 @@ var requireAccessToken = function(req, res, next) {
 
 var savedWords = [];
 
+// 각 기능에 매핑되는 3개의 핸들러: 각 유효한 액세스 토큰 전달 필요
 app.get('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "read" scope
-	 */
-	res.json({words: savedWords.join(' '), timestamp: Date.now()});
+	if (__.contains(req.access_token.scope, 'read')) {
+		res.json({words: savedWords.join(' '), timestamp: Date.now()});
+	} else {
+		res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="read"');
+		res.status(403).end();
+	}
 });
 
 app.post('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "write" scope
-	 */
-	if (req.body.word) {
-		savedWords.push(req.body.word);
+	if (__.contains(req.access_token.scope, 'write')) {
+		if (req.body.word) {
+			savedWords.push(req.body.word);
+		}
+		res.status(201).end();
+	} else {
+		res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="write"');
+		res.status(403).end();
 	}
-	res.status(201).end();
 });
 
 app.delete('/words', getAccessToken, requireAccessToken, function(req, res) {
-	/*
-	 * Make this function require the "delete" scope
-	 */
-	savedWords.pop();
-	res.status(204).end();
+	if (__.contains(req.access_token.scope, 'delete')) {
+		savedWords.pop();
+		res.status(204).end();
+	} else {
+		res.set('WWW-Authenticate', 'Bearer realm=localhost:9002, error="insufficient_scope", scope="delete"');
+		res.status(403).end();
+	}
 });
 
 var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+	console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
- 
+
